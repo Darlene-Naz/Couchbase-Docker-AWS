491033beab73d7f0c30702db4b797bdfd95883a2
Sherwyn Dsouza <sherwyndsouza1999@gmail.com>
2023-12-16 14:12:04 -0500

    Fixed ui

--- Secure-Highly-Available-Dockerized-App/app/index.js
+++ Secure-Highly-Available-Dockerized-App/app/index.js
@@ -1,21 +1,32 @@
-const express = require("express");
 const path = require("path");
+var cors = require('cors');
+var express = require('express');
+var couchbase = require('couchbase');
 const fs = require("fs");
-var cors = require('cors')
-var couchbase = require('couchbase')
 
-// Create a Couchbase Cluster connection
-const CB = {
-  host: process.env.CB_HOST || 'localhost',
-  username: process.env.CB_USER || 'Administrator',
-  password: process.env.CB_PASS || 'couchbase'
+var cluster;
+
+
+async function getFiveDocuments() {
+  const query = 'SELECT * from `wikipedia-data` limit 5;';
+  try {
+    const result = await cluster.query(query);
+    const documents = result.rows.map(row => row[Object.keys(row)[0]]);
+    return documents;
+  } catch (error) {
+    console.error('Error retrieving random documents:', error);
+    throw error;
+  }
 }
 
 const app = express();
 const PORT = process.env.PORT || 3000; // For testing locally
 const SERVER_ID = process.env.SERVER_ID || 2; // For testing locally
 
+app.use(cors('*'));
+
 app.use("/static", express.static(path.join(__dirname, "static")));
+
 
 app.get("/", (req, res) => {
   console.log({ server: SERVER_ID, port: PORT });
@@ -23,17 +34,34 @@ app.get("/", (req, res) => {
     path.join(__dirname, "/static/index.html"),
     "utf8"
   );
-  file = file.replace(
-    "Darlene",
-    '{ SERVER_ID: <span style="color:yellow">' +
-      SERVER_ID +
-      '</span>, PORT: <span style="color:yellow">' +
-      PORT +
-      "</span> }"
-  );
-  res.send(file);
+
+  getFiveDocuments().then((data) => {
+    let obj = {data: data};
+    file = file.replace(
+      "Darlene",
+      '{ SERVER_ID: <span style="color:yellow">' +
+        SERVER_ID +
+        '</span>, PORT: <span style="color:yellow">' +
+        PORT +
+        "</span>  }" + JSON.stringify(obj, null, 4)
+    );
+    res.send(file);
+  });
 });
 
-app.listen(PORT, function () {
+app.listen(PORT, async function () {
   console.log(`Server ${SERVER_ID} running at ${PORT}`);
+
+  const clusterConnStr = "couchbases://cb.x6jbuo5u3ob2my.cloud.couchbase.com";
+  const username = "app"; 
+  const password = process.env.PASSWORD || 'Elephant01!';
+  const bucketName = "wikipedia-data";
+  const scopeName = "_default";
+  const collectionName = "_default ";
+
+  cluster = await couchbase.connect(clusterConnStr, {
+  username: username,
+  password: password,
+  });
+  const bucket = cluster.bucket(bucketName);
 });
